88e93e675cf62b656f9af4c634fdc328e0ca0a0e
nikiforovall <alexey.nikiforovall@gmail.com>
2024-08-22 20:13:09 +0300

    add text summarization example

--- examples/TextSummarizationAndAggregationPipeline/Program.cs
+++ examples/TextSummarizationAndAggregationPipeline/Program.cs
@@ -5,64 +5,62 @@ using Microsoft.SemanticKernel.Connectors.OpenAI;
 using Shared;
 using static Shared.Steps;
 
-var builder = Host.CreateApplicationBuilder();
-var endpoint = builder.Configuration["AZURE_OPENAI_ENDPOINT"]!;
-var deployment = builder.Configuration["AZURE_OPENAI_GPT_NAME"]!;
-var key = builder.Configuration["AZURE_OPENAI_KEY"]!;
-var kernelBuilder = builder.Services.AddKernel();
-
-kernelBuilder.AddAzureOpenAIChatCompletion(deployment, endpoint, key);
-
-var services = builder.Build().Services;
-
-var kernel = services.GetRequiredService<Kernel>();
-
-var prompt = """
-    Please summarize the the following text in 20 words or less:
-    ${input}
-    """;
-var summarizationFunction = kernel.CreateFunctionFromPrompt(prompt);
-
+var (kernel, summarizationFunction) = Init();
 var path = Path.Combine(Directory.GetCurrentDirectory(), "..", "..", "Data");
-var files = Directory.EnumerateFiles(path).ToAsyncEnumerable();
 
-var filesCount = await files.CountAsync();
-
-var pipeline = files
+var pipeline = Directory
+    .EnumerateFiles(path)
+    .ToAsyncEnumerable()
+    .ReportProgress()
     .SelectAwait(ReadFile)
     .Where(IsValidFileForProcessing)
-    .Summarize(kernel, summarizationFunction)
-    .StoreBatch(batchSize: 10, path: Path.Combine(path, "summaries.txt"))
-    .ReportProgress(totalCount: filesCount)
+    .SelectAwait(Summarize)
+    .WriteResultToFile(path: Path.Combine(path, "summaries.txt"))
     .ForEachAsync(x => Console.WriteLine($"Processed {x.Name}"));
 
 await pipeline;
 
+static (Kernel kernel, KernelFunction summarizationFunction) Init()
+{
+    var builder = Host.CreateApplicationBuilder();
+    var endpoint = builder.Configuration["AZURE_OPENAI_ENDPOINT"]!;
+    var deployment = builder.Configuration["AZURE_OPENAI_GPT_NAME"]!;
+    var key = builder.Configuration["AZURE_OPENAI_KEY"]!;
+    var kernelBuilder = builder.Services.AddKernel();
+
+    kernelBuilder.AddAzureOpenAIChatCompletion(deployment, endpoint, key);
+
+    var services = builder.Build().Services;
+
+    var kernel = services.GetRequiredService<Kernel>();
+    var prompt = """
+        Please summarize the the following text in 20 words or less:
+        ${input}
+        """;
+    var summarizationFunction = kernel.CreateFunctionFromPrompt(prompt);
+
+    return (kernel, summarizationFunction);
+}
+
+async ValueTask<SummarizationPayload> Summarize(FilePayload file)
+{
+    var result = await summarizationFunction.InvokeAsync(
+        kernel,
+        new KernelArguments(new OpenAIPromptExecutionSettings() { MaxTokens = 400 }) { ["input"] = file.Content }
+    );
+
+    return new(file.Name, result.ToString());
+}
+
 public static class MyPipelineExtensions
 {
-    public static async IAsyncEnumerable<SummarizationPayload> Summarize(
-        this IAsyncEnumerable<FilePayload> values,
-        Kernel kernel,
-        KernelFunction function
-    )
-    {
-        await foreach (var value in values)
-        {
-            var result = await function.InvokeAsync(
-                kernel,
-                new KernelArguments(new OpenAIPromptExecutionSettings() { MaxTokens = 400 }) { ["input"] = value }
-            );
-
-            yield return new(value.Name, result.ToString());
-        }
-    }
-
-    public static async IAsyncEnumerable<SummarizationPayload> StoreBatch(
+    public static async IAsyncEnumerable<SummarizationPayload> WriteResultToFile(
         this IAsyncEnumerable<SummarizationPayload> values,
-        int batchSize,
         string path
     )
     {
+        const int batchSize = 10;
+
         using var streamWriter = new StreamWriter(path);
 
         await foreach (var batch in values.Buffer(batchSize))
@@ -78,8 +76,13 @@ public static class MyPipelineExtensions
         }
     }
 
-    public static async IAsyncEnumerable<T> ReportProgress<T>(this IAsyncEnumerable<T> values, int totalCount)
+    public static async IAsyncEnumerable<T> ReportProgress<T>(this IAsyncEnumerable<T> values)
     {
-        throw new NotImplementedException();
+        var totalCount = await values.CountAsync();
+
+        await foreach (var value in values)
+        {
+            yield return value;
+        }
     }
 }
